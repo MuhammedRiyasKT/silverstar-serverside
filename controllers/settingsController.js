e0c80dd61f198b091bf27e36ca63893fa6a4a58a
MuhammedRiyasKT <145179614+MuhammedRiyasKT@users.noreply.github.com>
2025-08-28 10:37:50 +0530

    Update settingsController.js

--- controllers/settingsController.js
+++ controllers/settingsController.js
@@ -1,34 +1,58 @@
-const Settings = require('../models/Settings');
+const mongoose = require("mongoose");
+const Settings = require("../models/Settings");
 
+// Get settings
 const getSettings = async (req, res) => {
   try {
     let settings = await Settings.findOne();
     if (!settings) {
-      settings = await Settings.create({ hotelLat: 10.841156, hotelLon: 76.109505 });
+      settings = await Settings.create({
+        hotelLat: mongoose.Types.Decimal128.fromString("10.841156"),
+        hotelLon: mongoose.Types.Decimal128.fromString("76.109505"),
+      });
     }
-    res.json({ success: true, data: settings });
+    res.json({
+      success: true,
+      data: {
+        hotelLat: settings.hotelLat.toString(),
+        hotelLon: settings.hotelLon.toString(),
+      },
+    });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ success: false, message: 'Server error' });
+    res.status(500).json({ success: false, message: "Server error" });
   }
 };
 
+// Update settings
 const updateSettings = async (req, res) => {
   try {
     const { hotelLat, hotelLon } = req.body;
     let settings = await Settings.findOne();
+
     if (!settings) {
-      settings = await Settings.create({ hotelLat, hotelLon });
+      settings = await Settings.create({
+        hotelLat: mongoose.Types.Decimal128.fromString(hotelLat.toString()),
+        hotelLon: mongoose.Types.Decimal128.fromString(hotelLon.toString()),
+      });
     } else {
-      settings.hotelLat = hotelLat;
-      settings.hotelLon = hotelLon;
+      settings.hotelLat = mongoose.Types.Decimal128.fromString(hotelLat.toString());
+      settings.hotelLon = mongoose.Types.Decimal128.fromString(hotelLon.toString());
       await settings.save();
     }
-    res.json({ success: true, data: settings });
+
+    res.json({
+      success: true,
+      data: {
+        hotelLat: settings.hotelLat.toString(),
+        hotelLon: settings.hotelLon.toString(),
+      },
+    });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ success: false, message: 'Server error' });
+    res.status(500).json({ success: false, message: "Server error" });
   }
 };
 
 module.exports = { getSettings, updateSettings };
+
