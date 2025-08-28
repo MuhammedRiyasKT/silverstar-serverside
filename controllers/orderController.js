409d444696747451db0a0ba6ebce7fc0c89d13b2
MuhammedRiyasKT <145179614+MuhammedRiyasKT@users.noreply.github.com>
2025-08-28 17:20:45 +0530

    Update orderController.js

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -462,7 +462,7 @@ const createOrder = async (req, res) => {
     }
 
     const distance = getDistanceFromLatLonInMeters(latitude, longitude, HOTEL_LAT, HOTEL_LON);
-    if (distance > 150) {
+    if (distance > 500) {
       return res.status(403).json({
         success: false,
         message: "You are outside the hotel. Orders can only be placed inside (within 50m).",
